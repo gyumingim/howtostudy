0e4396c44a50104f71fa2ec19c49f70a6d34d8ab
gyumingim <a316205045@gmail.com>
2024-09-03 09:02:12 +0900

    webcam but have bug

---
+++ OPENCV/240903_webcam.py
@@ -0,0 +1,23 @@
+import cv2 as cv
+
+gst_str = ("filesrc location=bottle-detection.mp4 ! decodebin ! video/x-raw ! queue ! videoconvert ! appsink")
+cap = cv.VideoCapture(gst_str, cv.CAP_GSTREAMER)
+
+while cap.isOpened():
+    ret, frame = cap.read()
+
+    if not ret:
+        print('fail')
+        break
+    
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+
+    cv.imshow('frame', gray)
+    if cv.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv.destroyAllWindows()
+
+# Extra: Play video in terminal with the following command:
+# gst-launch-1.0 v4l2src device=/dev/video0 ! videoconvert ! videoscale ! video/x-raw, width-640, height=360 ! autovideosink
